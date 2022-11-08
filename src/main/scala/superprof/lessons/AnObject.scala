5a6f5dd86ffd540feb794ac0ac999ee9eae8c304
hamsterofdeath <h-star@gmx.de>
2022-11-08 17:14:51 +0100

    cleanup

--- src/main/scala/superprof/lessons/AnObject.scala
+++ src/main/scala/superprof/lessons/AnObject.scala
@@ -1,5 +1,5 @@
 package superprof.lessons
 
 object AnObject {
-
+  def sum(a:Int,b:Int) = a+b
 }
--- src/test/scala/superprof/lessons/ObjectTest.scala
+++ src/test/scala/superprof/lessons/ObjectTest.scala
@@ -1,5 +1,10 @@
 package superprof.lessons
 
-class ObjectTest {
+import org.scalatest.flatspec.AnyFlatSpec
 
+class ObjectTest extends AnyFlatSpec {
+  "The object" should "add 1 + 1" in {
+    val sum = AnObject.sum(1,1)
+    assertResult(2)(sum)
+  }
 }
